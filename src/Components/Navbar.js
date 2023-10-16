043a3682a5e92068d0fbca88d447fb3b860ff087
SulakshanSiva <sulakshan1704@gmail.com>
2023-10-15 21:30:14 -0400

    started creating navbar, created basic outline

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,49 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  const navItems = [
+    {
+        link: "#",
+        name: "About"
+    },
+    {
+      link: "#",
+      name: "Career"
+    },
+    {
+      link: "#",
+      name: "Projects"
+    },
+  ];
+
+  const navList = navItems.map((item) =>
+    <li className=''>
+      <a href={item.link} className='hover:underline hover:decoration-orange-400'>{item.name}</a>
+    </li>
+  );
+
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className='bg-white text-black shadow-lg' >
+
+        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4'>
+
+          <span>
+            <a href="#" className='text-2xl text-orange-400'>Sulakshan Sivakumaran</a>
+          </span>
+        
+          <div className='hidden w-full md:block md:w-auto'>
+            <ul className='flex space-x-10 text-m'>
+              {navList}
+            </ul>
+          </div>
+
+        </div>
+
+      </nav>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
